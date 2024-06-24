3dee037a717683c0cf625b438bccb27621ec9908
hayapo <39045817+hayapo@users.noreply.github.com>
2024-06-24 21:20:39 +0900

    feature: initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
 import { Hono } from 'hono'
+import { trimTrailingSlash } from 'hono/trailing-slash'
 
 type Bindings = {
   [key in keyof CloudflareBindings]: CloudflareBindings[key]
 }
 
+const blogHost = "blog-hayapo-dev.pages.dev"
+
 const app = new Hono<{ Bindings: Bindings }>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.use(trimTrailingSlash())
+
+app.get('/blog', (c) => {
+  return fetch(`https://${blogHost}/blog`)
+})
+
+app.get('/blog/:entry', (c) => {
+  const { entry } = c.req.param()
+  return fetch(`https://${blogHost}/blog/${entry}`)
+})
+
+app.get('/site.webmanifest', (c) => {
+  return fetch(`https://${blogHost}/site.webmanifest`)
+})
+
+app.get("/:type{(?:public|_astro|favicon)}/:filename", async (c) => {
+  const { type, filename } = c.req.param()
+  return fetch(`https://${blogHost}/${type}/${filename}`)
 })
 
 export default app
