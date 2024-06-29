6ebcc87abec920d3ca75aa83d16484f7a5942319
hayapo <39045817+hayapo@users.noreply.github.com>
2024-06-30 06:03:17 +0900

    added homepage proxy routing

--- src/index.ts
+++ src/index.ts
@@ -5,28 +5,39 @@ type Bindings = {
   [key in keyof CloudflareBindings]: CloudflareBindings[key]
 }
 
+const baseHost = "hayapo.dev"
 const blogHost = "blog-hayapo-dev.pages.dev"
+const sampleHost = "subdirectory-hayapo-dev.pages.dev"
 
 const app = new Hono<{ Bindings: Bindings }>()
 
 app.use(trimTrailingSlash())
 
-app.get('/blog', (c) => {
+app.get('/', async (c) => {
+  return fetch(`https://${baseHost}`)
+})
+
+app.get('/:type{(?:public|_astro|favicon)}/:filename', async (c) => {
+  const { type, filename } = c.req.param()
+  return fetch(`https://${baseHost}/${type}/${filename}`)
+})
+
+app.get('/blog', async (c) => {
   return fetch(`https://${blogHost}/blog`)
 })
 
-app.get('/blog/:entry', (c) => {
+app.get('/blog/:entry', async (c) => {
   const { entry } = c.req.param()
-  return fetch(`https://${blogHost}/blog/${entry}`)
+    return fetch(`https://${blogHost}/blog/${entry}`)
 })
 
-app.get('/site.webmanifest', (c) => {
-  return fetch(`https://${blogHost}/site.webmanifest`)
+app.get('/blog/site.webmanifest', async  (c) => {
+  return fetch(`https://${blogHost}/blog/site.webmanifest`)
 })
 
-app.get("/:type{(?:public|_astro|favicon)}/:filename", async (c) => {
+app.get('/blog/:type{(?:public|_astro|favicon)}/:filename', async (c) => {
   const { type, filename } = c.req.param()
-  return fetch(`https://${blogHost}/${type}/${filename}`)
+  return fetch(`https://${blogHost}/blog/${type}/${filename}`)
 })
 
 export default app
