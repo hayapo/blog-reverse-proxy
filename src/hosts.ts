aad7f32b120bda6f973e6ffb9b419847d836d7ba
hayapo <39045817+hayapo@users.noreply.github.com>
2025-02-26 02:17:47 +0900

    ホスト群の定義をhosts.tsに移動

---
+++ src/hosts.ts
@@ -0,0 +1,4 @@
+export const baseHost = "hayapo.dev"
+export const blogHost = "blog-hayapo-dev.pages.dev"
+export const pomodoroHost = "pomodoro-54z.pages.dev"
+export const sampleHost = "subdirectory-hayapo-dev.pages.dev"
